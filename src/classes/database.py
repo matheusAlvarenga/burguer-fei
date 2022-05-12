d704777922e3c0cf3789354b6f3ae64b2bb9fdff
Matheus Alvarenga <matheus.alvarenga@natahous.com>
2022-05-11 22:02:04 -0300

    feat(classes): :sparkles: add new method find one to database

--- src/classes/database.py
+++ src/classes/database.py
@@ -1,3 +1,6 @@
+from re import findall
+
+
 class Database:
     def __init__(self, filePath):
         self.filePath = filePath
@@ -10,6 +13,13 @@ class Database:
     def delete(self):
         print('deleted')
 
-    def findOne(self):
+    def findOne(self, query):
+        with open(self.filePath, mode='r') as database_file:
+            lines = database_file.readlines()
+            for line in lines:
+                if(findall(query, line)):
+                    return line
+
+            return False
         print('found')
     
