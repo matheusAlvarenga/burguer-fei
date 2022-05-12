bb884b1191f5388399680a7b21aeed8e35d7b765
matheusAlvarenga <matheus.alvarenga@natahouse.com>
2022-05-12 19:26:42 -0300

    fix(classes): :bug: create database file if doesnt exists

--- src/classes/database.py
+++ src/classes/database.py
@@ -1,9 +1,14 @@
 from re import findall
+from os.path import exists, dirname
+from os import makedirs
 
 
 class Database:
     def __init__(self, filePath):
         self.filePath = filePath
+        if(not exists(filePath)):
+            makedirs(dirname(filePath), exist_ok=True)
+            open(filePath, mode='x')
 
     def create(self, data):
         with open(self.filePath, mode='a') as database_file:
