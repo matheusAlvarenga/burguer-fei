033deee040000f8a6627e6941a65e366b538cd02
matheusAlvarenga <matheus.alvarenga@natahouse.com>
2022-05-12 19:27:28 -0300

    feat(main): :sparkles: clear terminal and wait for user input when user already exists

--- src/main.py
+++ src/main.py
@@ -1,17 +1,22 @@
 from printers.mainmenu import renderMainMenu
 from services.user import signIn
+from utils.clear_terminal import clear
 
 
 def main():
     selected = None
 
     while(selected != '0'):
+        clear()
         selected = renderMainMenu()
 
         if(selected == '1'):
             user = signIn()
             if(not user):
-                print('Já existe um usuário')
+                clear()
+                print('Já existe um usuário com esse CPF!')
+                input('Pressione enter para voltar ao menu.')
+                continue
 
 
 if __name__ == "__main__":
