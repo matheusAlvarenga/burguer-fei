d3335d93b0b830bb978a86a6a499b8b53b967981
matheusAlvarenga <matheus.alvarenga@natahouse.com>
2022-05-13 19:21:42 -0300

    feat(parsers): :sparkles: create string to user parser

--- src/parsers/user.py
+++ src/parsers/user.py
@@ -1,2 +1,12 @@
 def userToString(user):
     return f'{user["document"]},{user["name"]},{user["password"]}'
+
+
+def stringToUser(string):
+    user_array = string.strip().split(',')
+
+    return {
+        'document': user_array[0],
+        'name': user_array[1],
+        'password': user_array[2]
+    }
