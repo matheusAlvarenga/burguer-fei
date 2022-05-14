91d6fd40b87667f8fcce38f33c83ef21cba83b83
matheusAlvarenga <matheus.alvarenga@natahouse.com>
2022-05-14 11:33:11 -0300

    feat(printers): :sparkles: create printer for orders table

---
+++ src/printers/orders.py
@@ -0,0 +1,39 @@
+from utils.clear_terminal import clear
+from printers.table import print_table
+
+
+def print_orders(products):
+    product_array = []
+
+    mapped_products = list(map(lambda product: {
+        'id': product['id'],
+        'product_name': product['product_name'],
+        'product_price': product['product_price'],
+        'quantity': product['quantity'],
+        'total_price': product['total_price']
+    }, products))
+
+    for product in mapped_products:
+        product_array.append(list(product.values()))
+
+    headers = [
+        "Código",
+        "Produto",
+        "Preço Unitário",
+        "Quantidade",
+        "Preço Total"
+    ]
+
+    clear()
+
+    print(
+        'Digite o código do produto que você deseja remover ou 0 para cancelar: '
+    )
+
+    print()
+
+    print_table(headers=headers, body=product_array)
+
+    print()
+
+    return input('Código: ')
